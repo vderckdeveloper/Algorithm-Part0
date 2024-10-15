feb0603f89c34debc9a43f8b51284b8aeeca54ba
vderckdeveloper <vderckdeveloper@gmail.com>
2024-10-15 19:14:49 +0900

    feat: 369 game algorithm

---
+++ 369game.js
@@ -0,0 +1,13 @@
+const order = 29423;
+
+function solution(order) {
+    const orderStr = order.toString();
+
+    let count = 0;
+    for (let i = 0; i < orderStr.length; i++) {
+        const char = orderStr[i];
+        if (char === '3' || char === '6' || char === '9') count++;
+    }
+
+    return count;
+}
