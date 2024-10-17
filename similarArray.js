4e9b556b66f2c4f5ed360fe087ea9d0c37ac9ddc
vderckdeveloper <vderckdeveloper@gmail.com>
2024-10-17 12:57:47 +0900

    refractor: better expression of algorithm

--- similarArray.js
+++ similarArray.js
@@ -3,15 +3,7 @@ const s2 = ["com", "b", "d", "p", "c"];
 
 
 function solution(s1, s2) {
-    let count = 0;
-    for (let i = 0; i < s1.length; i++) {
-        const s1Element = s1[i];
-        for (let j = 0; j < s2.length; j++) {
-            const s2Element = s2[j];
-            if(s1Element === s2Element) count++;
-        }
-    }
-    return count;
+    return s1.filter(el => s2.includes(el));
 }
 
 solution(s1, s2);
