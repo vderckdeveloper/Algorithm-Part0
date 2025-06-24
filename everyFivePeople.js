f43d344e5a2042eb388b7559af47feefce441b51
vderckdeveloper <vderckdeveloper@gmail.com>
2025-06-24 15:51:20 +0900

    feat: every five people algorithm

---
+++ everyFivePeople.js
@@ -0,0 +1,3 @@
+const names = ["nami", "ahri", "jayce", "garen", "ivern", "vex", "jinx", "nami", "ahri", "jayce", "garen", "ivern", "vex", "jinx"];
+
+const solution = names => names.filter((_,i) => i%5 === 0);
