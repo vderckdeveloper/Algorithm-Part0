48dc09b8b2358c6af74654e89d6aeaf1cbd11fca
vderckdeveloper <vderckdeveloper@gmail.com>
2025-07-08 16:23:55 +0900

    feat: udlr algorithm

---
+++ udlr.js
@@ -0,0 +1,9 @@
+const str_list = ["u", "u", "l", "r"];
+
+const solution = str_list => {
+    for (let i = 0; i < str_list.length; i++) {
+        if (str_list[i] === 'l') return str_list.slice(0, i);
+        if (str_list[i] === 'r') return str_list.slice(i+1);
+    }
+    return [];
+}
