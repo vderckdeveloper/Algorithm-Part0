f98b4e5975a538e987c1acea30481f402c7eecec
vderckdeveloper <vderckdeveloper@gmail.com>
2025-09-22 10:44:14 +0900

    feat: switch number algorithm

---
+++ switchNumber.js
@@ -0,0 +1,20 @@
+const numbers = 'onetwothreefourfivesixseveneightnine';
+
+const solution = (numbers) => {
+   const obj = {
+    zero: "0",
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9"
+  };
+
+  return parseInt(
+    numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, match => obj[match])
+  );
+}
