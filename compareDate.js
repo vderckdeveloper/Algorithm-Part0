02c32b34c16d9b87bc70711bb615a169ebe6abd7
vderckdeveloper <vderckdeveloper@gmail.com>
2025-09-24 19:05:43 +0900

    feat: compare date algorithm

---
+++ compareDate.js
@@ -0,0 +1,7 @@
+const solution = (date1, date2) => {
+    const yearComparsion = date1[0] < date2[0];
+    const monthComparsion = date1[0] === date2[0] && date1[1] < date2[1];
+    const dateComparsion = date1[0] === date2[0] && date1[1] === date2[1] && date1[2] < date2[2];
+    if (yearComparsion || monthComparsion || dateComparsion) return 1
+    return 0;
+}
