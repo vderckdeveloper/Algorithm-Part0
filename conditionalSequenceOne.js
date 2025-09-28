bf53a64443e17b960034c3bcb60e1e04f4a429cd
vderckdeveloper <vderckdeveloper@gmail.com>
2025-09-28 09:03:44 +0900

    feat: conditional sequence one algorithm

---
+++ conditionalSequenceOne.js
@@ -0,0 +1,26 @@
+const arr = [1, 2, 3, 100, 99, 98];
+
+const solution = (arr) => {
+    let prevArr = arr; // arr(0)
+    let count = 0;
+
+    while (true) {
+        const nextArr = prevArr.map(v => {
+            if (v >= 50 && v % 2 === 0) {
+                return v / 2;
+            } else if (v < 50 && v % 2 === 1) {
+                return v * 2 + 1;
+            }
+            return v; 
+        });
+
+        if (prevArr.every((el, idx) => el === nextArr[idx])) {
+            return count;
+        }
+
+        prevArr = nextArr;
+        count++;
+    }
+};
+
+solution(arr);
