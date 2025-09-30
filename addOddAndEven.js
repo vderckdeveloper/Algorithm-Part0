17a2c1ed382ae5331923409ae666dd16f5a3a610
vderckdeveloper <vderckdeveloper@gmail.com>
2025-09-30 09:37:41 +0900

    feat: add odd and even algorithm

---
+++ addOddAndEven.js
@@ -0,0 +1,7 @@
+
+
+const solution = num_list => {
+   const oddSum = num_list.reduce((cum, cur) => cur % 2 !== 0 ? cum.toString() + cur.toString() : cum, 0);
+   const evenSum = num_list.reduce((cum, cur) => cur % 2 === 0 ? cum.toString() + cur.toString() : cum, 0);
+   return Number(oddSum) + Number(evenSum);
+}
