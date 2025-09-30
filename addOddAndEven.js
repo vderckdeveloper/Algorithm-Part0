ee6bd47c2e1c64cb5610b609db14807772d67d2e
vderckdeveloper <vderckdeveloper@gmail.com>
2025-09-30 09:38:29 +0900

    chore: remove unnecessary lines

--- addOddAndEven.js
+++ addOddAndEven.js
@@ -1,5 +1,3 @@
-
-
 const solution = num_list => {
    const oddSum = num_list.reduce((cum, cur) => cur % 2 !== 0 ? cum.toString() + cur.toString() : cum, 0);
    const evenSum = num_list.reduce((cum, cur) => cur % 2 === 0 ? cum.toString() + cur.toString() : cum, 0);
